1b419a4e55ad92530b2a609fded78de3932fcb28
storm654321 <storm654321@gmail.com>
2017-08-23 13:40:20 +0800

    加入專案檔案。

---
+++ Homework7/Form1.Designer.cs
@@ -0,0 +1,201 @@
+﻿namespace Homework7
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 設計工具所需的變數。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清除任何使用中的資源。
+        /// </summary>
+        /// <param name="disposing">如果應該處置 Managed 資源則為 true，否則為 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 設計工具產生的程式碼
+
+        /// <summary>
+        /// 此為設計工具支援所需的方法 - 請勿使用程式碼編輯器修改
+        /// 這個方法的內容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.button6 = new System.Windows.Forms.Button();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.label1 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.pictureBox1.Location = new System.Drawing.Point(35, 112);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(780, 704);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.pictureBox2.Location = new System.Drawing.Point(865, 112);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(780, 704);
+            this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox2.TabIndex = 1;
+            this.pictureBox2.TabStop = false;
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("新細明體", 36F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.button1.Location = new System.Drawing.Point(294, 13);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(220, 93);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "載入圖片";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Font = new System.Drawing.Font("新細明體", 21.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.button2.Location = new System.Drawing.Point(865, 12);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(144, 43);
+            this.button2.TabIndex = 3;
+            this.button2.Text = "膨脹";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Font = new System.Drawing.Font("新細明體", 21.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.button3.Location = new System.Drawing.Point(865, 61);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(144, 43);
+            this.button3.TabIndex = 4;
+            this.button3.Text = "侵蝕";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button4
+            // 
+            this.button4.Font = new System.Drawing.Font("新細明體", 21.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.button4.Location = new System.Drawing.Point(1097, 12);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(144, 43);
+            this.button4.TabIndex = 5;
+            this.button4.Text = "Opening";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // button5
+            // 
+            this.button5.Font = new System.Drawing.Font("新細明體", 21.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.button5.Location = new System.Drawing.Point(1097, 63);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(144, 43);
+            this.button5.TabIndex = 6;
+            this.button5.Text = "Closing";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // button6
+            // 
+            this.button6.Font = new System.Drawing.Font("新細明體", 36F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.button6.Location = new System.Drawing.Point(1300, 11);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(345, 93);
+            this.button6.TabIndex = 7;
+            this.button6.Text = "儲存結果圖片";
+            this.button6.UseVisualStyleBackColor = true;
+            this.button6.Click += new System.EventHandler(this.button6_Click);
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Items.AddRange(new object[] {
+            "3X3",
+            "5X5",
+            "7X7",
+            "9X9",
+            "11X11",
+            "13X13",
+            "15X15",
+            "17X17",
+            "19X19",
+            "21X21",
+            "23X23"});
+            this.comboBox1.Location = new System.Drawing.Point(712, 29);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(121, 20);
+            this.comboBox1.TabIndex = 8;
+            this.comboBox1.SelectedIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("新細明體", 21.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.label1.Location = new System.Drawing.Point(548, 26);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(158, 29);
+            this.label1.TabIndex = 9;
+            this.label1.Text = "膨脹倍率：";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1679, 828);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.button6);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.pictureBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Button button6;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ Homework7/Form1.cs
@@ -0,0 +1,242 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Drawing.Imaging;
+namespace Homework7
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        Bitmap a, b;
+        private void button1_Click(object sender, EventArgs e)
+        {
+            FileDialog tomato =new OpenFileDialog();
+            if(tomato.ShowDialog()==DialogResult.OK)
+            {
+                a = new Bitmap(tomato.FileName);
+                b = new Bitmap(tomato.FileName);
+                pictureBox1.Image = a;
+                pictureBox2.Image = b;
+            }
+        }
+        private void button2_Click(object sender, EventArgs e)//膨脹按鈕
+        {
+            if (pictureBox1.Image == null)
+            {
+                MessageBox.Show("請先Load圖片");
+                return;
+            }
+            b = big(b);
+            pictureBox2.Image = b;
+        }
+        private void button3_Click(object sender, EventArgs e)//侵蝕按鈕
+        {
+            if (pictureBox1.Image == null)
+            {
+                MessageBox.Show("請先Load圖片");
+                return;
+            }
+            b = small(b);
+            pictureBox2.Image = b;
+        }
+        private void button4_Click(object sender, EventArgs e)//open按鈕
+        {
+            if (pictureBox1.Image == null)
+            {
+                MessageBox.Show("請先Load圖片");
+                return;
+            }
+            b = small(b);
+            b = big(b);
+            pictureBox2.Image = b;
+        }
+        private void button5_Click(object sender, EventArgs e)//closing按鈕
+        {
+            if (pictureBox1.Image == null)
+            {
+                MessageBox.Show("請先Load圖片");
+                return;
+            }
+            b = big(b);
+            b = small(b);
+            pictureBox2.Image = b;
+        }
+        private void button6_Click(object sender, EventArgs e)//儲存圖片
+        {
+            if (pictureBox1.Image == null)
+            {
+                MessageBox.Show("請先Load圖片");
+                return;
+            }
+
+            else
+            {
+                SaveFileDialog saveFileDialog1 = new SaveFileDialog();
+                saveFileDialog1.Filter = "所有檔案|*.*|BMP File|*.bmp|JPEG File|*.jpg|GIF File|*.gif|PNG File|*.png|TIFF File|*.tiff";
+                saveFileDialog1.FilterIndex = 3;
+                if (saveFileDialog1.ShowDialog() == System.Windows.Forms.DialogResult.OK && saveFileDialog1.FileName != "")
+                {
+                    Bitmap processedBitmap = b;
+                    switch (saveFileDialog1.FilterIndex)
+                    {
+                        case 1:
+                            processedBitmap.Save(saveFileDialog1.FileName, System.Drawing.Imaging.ImageFormat.Jpeg);
+                            break;
+                        case 2:
+                            processedBitmap.Save(saveFileDialog1.FileName, System.Drawing.Imaging.ImageFormat.Bmp);
+                            break;
+                        case 3:
+                            processedBitmap.Save(saveFileDialog1.FileName, System.Drawing.Imaging.ImageFormat.Gif);
+                            break;
+                        case 4:
+                            processedBitmap.Save(saveFileDialog1.FileName, System.Drawing.Imaging.ImageFormat.Png);
+                            break;
+                        case 5:
+                            processedBitmap.Save(saveFileDialog1.FileName, System.Drawing.Imaging.ImageFormat.Tiff);
+                            break;
+                    }
+                }
+            }
+        }
+        Bitmap big(Bitmap input)//膨脹
+        {
+            int[,,] source = GetRGBData(input);
+            int[,,] output = GetRGBData(input);
+            int filter_size = comboBox1.SelectedIndex*2+3,filter_temp =filter_size/ 2,flag;
+            for(int i=filter_temp;i<a.Width- filter_temp; i++)
+            {
+                for(int j= filter_temp ; j < a.Height - filter_temp; j++)
+                {
+                    flag = 0;
+                    for (int inner_i = i - filter_temp; inner_i <= i + filter_temp; inner_i++)
+                        for (int inner_j = j - filter_temp; inner_j <= j + filter_temp; inner_j++)
+                            if(source[inner_i,inner_j,0]<150)
+                                flag = 1;
+                    if (flag == 1)
+                        for (int k = 0; k < 3; k++)
+                            if(flag==1)
+                                output[i, j, k] = 0;
+                }
+
+            }
+            return setRGBData(output);
+        }
+        Bitmap small(Bitmap input) //侵蝕
+        {
+            int[,,] source = GetRGBData(input);
+            int[,,] output = new int[input.Width, input.Height,3];
+            for (int i = 0; i < a.Width; i++)
+                for (int j = 0; j < a.Height; j++)
+                    for (int k = 0; k < 3; k++)
+                        output[i, j, k] = 255;
+            int filter_size = comboBox1.SelectedIndex*2+3, filter_temp = filter_size / 2, flag;
+            int filter_sqrt = filter_size * filter_size;
+            for (int i = filter_temp; i < a.Width - filter_temp; i++)
+            {
+                for (int j = filter_temp; j < a.Height - filter_temp; j++)
+                {
+                    flag = 0;
+                    for (int inner_i = i - filter_temp; inner_i <= i + filter_temp; inner_i++)
+                        for (int inner_j = j - filter_temp; inner_j <= j + filter_temp; inner_j++)
+                            if (source[inner_i, inner_j, 0] < 150)
+                                flag ++;
+                    if (flag == filter_sqrt)
+                        for (int k = 0; k < 3; k++)
+                            output[i, j, k] = 0;
+                }
+
+            }
+            return setRGBData(output);
+        }
+        public static int[,,] GetRGBData(Bitmap bitImg)
+        {
+            int height = bitImg.Height;
+            int width = bitImg.Width;
+            //locking
+            BitmapData bitmapData = bitImg.LockBits(new Rectangle(0, 0, width, height), ImageLockMode.ReadOnly, PixelFormat.Format24bppRgb);
+            // get the starting memory place
+            IntPtr imgPtr = bitmapData.Scan0;
+            //scan width
+            int stride = bitmapData.Stride;
+            //scan ectual
+            int widthByte = width * 3;
+            // the byte num of padding
+            int skipByte = stride - widthByte;
+            //set the place to save values
+            int[,,] rgbData = new int[width, height, 3];
+            #region
+            unsafe//專案－＞屬性－＞建置－＞容許Unsafe程式碼須選取。
+            {
+                byte* p = (byte*)(void*)imgPtr;
+                for (int j = 0; j < height; j++)
+                {
+                    for (int i = 0; i < width; i++)
+                    {
+                        //B channel
+                        rgbData[i, j, 2] = p[0];
+                        p++;
+                        //g channel
+                        rgbData[i, j, 1] = p[0];
+                        p++;
+                        //R channel
+                        rgbData[i, j, 0] = p[0];
+                        p++;
+                    }
+                    p += skipByte;
+                }
+            }
+            bitImg.UnlockBits(bitmapData);
+            #endregion
+            return rgbData;
+        }
+
+
+
+        public static Bitmap setRGBData(int[,,] rgbData)
+        {
+            Bitmap bitImg;
+            int width = rgbData.GetLength(0);
+            int height = rgbData.GetLength(1);
+            bitImg = new Bitmap(width, height, PixelFormat.Format24bppRgb);// 24bit per pixel 8x8x8
+            //locking
+            BitmapData bitmapData = bitImg.LockBits(new Rectangle(0, 0, width, height), ImageLockMode.WriteOnly, PixelFormat.Format24bppRgb);
+            //get image starting place
+            IntPtr imgPtr = bitmapData.Scan0;
+            //image scan width
+            int stride = bitmapData.Stride;
+            int widthByte = width * 3;
+            int skipByte = stride - widthByte;
+            #region
+            unsafe
+            {
+                byte* p = (byte*)(void*)imgPtr;
+                for (int j = 0; j < height; j++)
+                {
+                    for (int i = 0; i < width; i++)
+                    {
+                        p[0] = (byte)rgbData[i, j, 2];
+                        p++;
+                        p[0] = (byte)rgbData[i, j, 1];
+                        p++;
+                        p[0] = (byte)rgbData[i, j, 0];
+                        p++;
+                    }
+                    p += skipByte;
+                }
+
+            }
+            bitImg.UnlockBits(bitmapData);
+            #endregion
+            return bitImg;
+        }
+    }
+}
